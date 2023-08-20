52a6b417ef6e9d3f2af84ddceb920d32c005cebb
Gurudatta B S <96541752+GURUDATTA17@users.noreply.github.com>
2023-08-20 22:16:57 +0530

    Update diseaseprediction.py

--- diseaseprediction.py
+++ diseaseprediction.py
@@ -1,42 +1,9 @@
+#DISEASE PREDICTION (USING BASIC ML ALGORITHMS) USING PYTHON
+#PROJECT BY GURUDATTA BS< CHOWDAM JAGANNATH< K NARENDRA
 
 from tkinter import *
 import numpy as np
 import pandas as pd
-
-#List of the symptoms is listed here in list l1.
-
-l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
-'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
-'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
-'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
-'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
-'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
-'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
-'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
-'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
-'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
-'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
-'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
-'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
-'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
-'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
-'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
-'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
-'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
-'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
-'yellow_crust_ooze']
-
-#List of Diseases is listed in list disease.
-
-disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction',
-'Peptic ulcer diseae','AIDS','Diabetes','Gastroenteritis','Bronchial Asthma','Hypertension',
-' Migraine','Cervical spondylosis',
-'Paralysis (brain hemorrhage)','Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A',
-'Hepatitis B','Hepatitis C','Hepatitis D','Hepatitis E','Alcoholic hepatitis','Tuberculosis',
-'Common Cold','Pneumonia','Dimorphic hemmorhoids(piles)',
-'Heartattack','Varicoseveins','Hypothyroidism','Hyperthyroidism','Hypoglycemia','Osteoarthristis',
-'Arthritis','(vertigo) Paroymsal  Positional Vertigo','Acne','Urinary tract infection','Psoriasis',
-'Impetigo']
 
 l2=[]
 
@@ -165,7 +132,7 @@ def randomforest():
         t2.delete("1.0", END)
         t2.insert(END, "Not Found")
 
-
+"""
 def NaiveBayes():
     from sklearn.naive_bayes import GaussianNB
     gnb = GaussianNB()
@@ -198,7 +165,7 @@ def NaiveBayes():
     else:
         t3.delete("1.0", END)
         t3.insert(END, "Not Found")
-
+"""
 # GUI stuff..............................................................................
         
 root = Tk()
@@ -293,11 +260,11 @@ dst.grid(row=8, column=3,padx=10)
 rnf = Button(root, text="Prediction 2", command=randomforest,bg="White",fg="Black")
 rnf.config(font=("Times",15,"bold italic"))
 rnf.grid(row=9, column=3,padx=10)
-
+"""
 lr = Button(root, text="Prediction 3", command=NaiveBayes,bg="Green",fg="Black")
 lr.config(font=("Times",15,"bold italic"))
 lr.grid(row=10, column=3,padx=10)
-
+"""
 
 t1 = Text(root, height=1, width=40,bg="white",fg="red")
 t1.config(font=("Times",15,"bold italic"))
@@ -306,9 +273,44 @@ t1.grid(row=15, column=1, padx=10)
 t2 = Text(root, height=1, width=40,bg="White",fg="Blue")
 t2.config(font=("Times",15,"bold italic"))
 t2.grid(row=17, column=1 , padx=10)
-
+"""
 t3 = Text(root, height=1, width=40,bg="white",fg="white")
 t3.config(font=("Times",15,"bold italic"))
 t3.grid(row=19, column=1 , padx=10)
+"""
+#List of the symptoms is listed here in list l1.
+
+l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
+'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
+'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
+'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
+'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
+'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
+'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
+'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
+'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
+'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
+'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
+'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
+'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
+'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
+'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
+'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
+'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
+'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
+'yellow_crust_ooze']
+
+#List of Diseases is listed in list disease.
+
+disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction',
+'Peptic ulcer diseae','AIDS','Diabetes','Gastroenteritis','Bronchial Asthma','Hypertension',
+' Migraine','Cervical spondylosis',
+'Paralysis (brain hemorrhage)','Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A',
+'Hepatitis B','Hepatitis C','Hepatitis D','Hepatitis E','Alcoholic hepatitis','Tuberculosis',
+'Common Cold','Pneumonia','Dimorphic hemmorhoids(piles)',
+'Heartattack','Varicoseveins','Hypothyroidism','Hyperthyroidism','Hypoglycemia','Osteoarthristis',
+'Arthritis','(vertigo) Paroymsal  Positional Vertigo','Acne','Urinary tract infection','Psoriasis',
+'Impetigo']
 
 root.mainloop()
